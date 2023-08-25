50a87b9b3ba04c3e0064898383e33dbadc4bd8d6
black-devil-369 <ajaychauhan1262@gmail.com>
2023-08-25 13:45:11 +0530

    switch statement using js

---
+++ Fundamentals/switch.js
@@ -0,0 +1,70 @@
+
+let a  = new Date();
+switch(a.getDay()){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Thuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("End! Switch Case");
+        break;
+}
+console.log("Current Month is");
+switch (a.getMonth()) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Agusth");
+        break;
+    case 9:
+        console.log("September")
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("End! Switch-Case");
+        break;
+}

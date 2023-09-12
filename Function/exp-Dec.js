c45d22d7da244f48b1d731279df9e9ad9c3c9e19
black-devil-369 <ajaychauhan1262@gmail.com>
2023-09-12 22:23:14 +0530

    function Expression and Function Declaration in js

---
+++ Function/exp-Dec.js
@@ -0,0 +1,22 @@
+// function expression syntax
+const rect = function(height,witdh){
+    return height*witdh;
+};
+console.log(rect(8,9));
+
+// another example
+const fn = function(){
+    return "Hello! Coder";
+}
+console.log(fn());
+
+const fu = ()=>{
+    return "jai Maa kali";
+}
+console.log(fu());
+
+// declaration function 
+function fn1(){
+    return "I am good";
+}
+console.log(fn1());

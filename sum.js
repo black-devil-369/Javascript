39a7080249376d17913bd25795007ce90b462f61
black-devil-369 <ajaychauhan1262@gmail.com>
2023-09-23 07:37:26 +0530

    done

---
+++ sum.js
@@ -0,0 +1,22 @@
+// const blance = new Number(100);
+// const b = blance.toPrecision();
+// console.log(b.length)
+// console.log(Math.abs(-52).toString().length);
+// const num = [1,6,8];
+// const num1 = [4,2];
+// let arr = [];
+// for(const i of num){
+//     arr.push(i);
+// }
+// for(const i of num1){
+//     arr.push(i);
+// }
+
+const name = "Ajay";
+const arr =[];
+for(const i of name){
+    arr.push(i)
+}
+console.log(arr);
+const arr1 = Array.from(name);
+console.log(arr1);
